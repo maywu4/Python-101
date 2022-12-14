188941c365240183c9cf8cd4cc56573c4fc41dd3
May Wu <maywu4@gmail.com>
2022-12-14 18:50:05 -0500

    first commit

---
+++ 1.input.py
@@ -0,0 +1,10 @@
+# Prompt for input()
+# Formatting printing
+
+name = input("What is your name?\n")
+# print(name)
+
+# print("Hi, " + name + ".")
+# print("Hi, %s." % name)
+# print("Hi, {0}.".format(name))
+print(f'Hi, {name}.')
---
+++ 2.arithmetic.py
@@ -0,0 +1,9 @@
+a = "a"
+b = "b"
+an = "an"
+
+print(b + an)
+print(b + a*7)
+print(b + an*2 + a)
+
+print("$1" + ",000"*3)
