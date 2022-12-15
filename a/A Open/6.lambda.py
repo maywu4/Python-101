3cd709ff9322e3af6f4f607955333b22dcd031b9
May Wu <maywu4@gmail.com>
2022-12-15 15:32:26 -0500

    12/15

---
+++ a/A Open/6.lambda.py
@@ -0,0 +1,9 @@
+def xor(left, right):
+    return left != right
+
+# xor = (left, right) => left != right
+xor = lambda left, right: left != right
+
+
+print_powers_of = lambda base, power: base**power
+print(print_powers_of(2,5))
