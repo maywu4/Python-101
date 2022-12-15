c99b67b8a7ba10c4c529f001ffc372dde60c8302
May Wu <maywu4@gmail.com>
2022-12-14 20:56:52 -0500

    12/14

--- a/A Open/2.arithmetic.py
+++ a/A Open/2.arithmetic.py
@@ -2,8 +2,30 @@ a = "a"
 b = "b"
 an = "an"
 
-print(b + an)
-print(b + a*7)
-print(b + an*2 + a)
+# print(b + an)
+# print(b + a*7)
+# print(b + an*2 + a)
 
-print("$1" + ",000"*3)
+# print("$1" + ",000"*3)
+
+x = 25
+y = 17.0
+
+# print(x)
+# print(y)
+
+# print(x+y)
+# print(x*y)
+
+# print(x // y)
+# print(x % y)
+# print(f'The result is {int(x // y)} remainder {int(x % y)}')
+
+# print(x ** 2)
+# print(y ** 2)
+
+x = 25
+y = 17.6
+# print(x * y) #float rounding errors
+
+print(5 / 2)
---
+++ a/A Open/3.basics.py
@@ -0,0 +1,8 @@
+# print('Hello, World')
+# print("It's raining!")
+# print('''Python's replacement for null is None. It is used to indicate a variable has no value.
+
+# None is very special because it is actually an object (of type NoneType). That means it can be used wherever other objects are used''')
+
+a = None
+print(a)
