6f39688351571809c72e7d02caea14d218d4dd7f
May Wu <maywu4@gmail.com>
2022-12-15 17:03:41 -0500

    lists

--- a/A Open/0.problems.py
+++ a/A Open/0.problems.py
@@ -41,3 +41,16 @@ print(valid_zip_code(zip3))     # '01237-1238'
 print(valid_zip_code(zip4))     # "The zip code you entered is invalid"
 print(valid_zip_code(zip5))     # "The zip code you entered is invalid"
 print(valid_zip_code(zip6))     # "The zip code you entered is invalid"
+
+
+
+# Insertion Sort
+# Create a function that uses the insertion sort algorithm to sort the list.
+
+# Write your function, here.
+
+
+
+print(insertion_sort([55, 21, 5, 3, 6, 95])) #> [3, 5, 6, 21, 55, 95]
+print(insertion_sort([4, 1, 0, 3, 8, 9])) #> [0, 1, 3, 4, 8, 9]
+print(insertion_sort([1, 4, 3, 0, 3, 0, 2, 8])) #> [0, 0, 1, 2, 3, 3, 4, 8]
--- a/A Open/7.lists.py
+++ a/A Open/7.lists.py
@@ -1,2 +1,67 @@
-empty = []
-print(empty)
+# ordered, mutable
+
+# empty = []
+# print(empty)
+
+# friends = ['Monica', 'Chandler', 'Joey', 'Phoebe', 'Rachel', 'Ross']
+# print(friends)
+
+# print(friends[0])
+# # print(friends[15])
+
+# print(friends[-1]) #returns 1 item
+# print(friends[1:-1]) #returns items in list
+# print(friends[-1:]) #returns 1 item in list
+# print(friends[1::2]) #returns every other item starting at idx 1 til end of list
+
+
+# print(len(friends))
+# print('Joey' in friends)
+
+# scores = [159, 210, 188, 76]
+# print(scores)
+
+# teamScore = sum(scores)
+# print(teamScore)
+
+# highestScore = max(scores)
+# print(highestScore)
+
+# lowestScore = min(scores)
+# print(lowestScore)
+
+# average = sum(scores)/len(scores)
+# print(average)
+
+# rankedScores = sorted(scores, reverse=True)
+# print(rankedScores)
+
+# supplies = ['crayons', 'pencils', 'paper', 'Kleenex', 'eraser']
+# print(supplies)
+
+# supplies.append('markers')
+# print(supplies)
+
+# supplies.remove('crayons')
+# print(supplies)
+
+# supplies.sort()
+# print(supplies)
+
+# supplies.sort(key=str.lower)
+# print(supplies)
+
+colors = ['red', 'orange', 'blue', 'pink']
+alphabetical = sorted(colors)
+print(colors)
+print(alphabetical)
+
+alphabetical = sorted(colors, reverse=True)
+print(alphabetical)
+
+reversedColors = reversed(colors)
+reverseAlpha = reversed(alphabetical)
+print(reversedColors)
+print(reverseAlpha)
+print(list(reversedColors))
+print(list(reverseAlpha))
