44b895023eac2258b3059272001c5b6d5a8104ef
May Wu <maywu4@gmail.com>
2022-12-15 22:19:57 -0500

    ranges

---
+++ a/A Open/9.ranges.py
@@ -0,0 +1,13 @@
+nums = range(10)
+# print(nums)
+# print(list(nums))
+
+counters = range(1, 11)
+# print(list(counters))
+
+fives = range(0, 50, 5)
+# print(list(fives))
+
+items = ['a', 'b', 'c']
+for i in range(len(items)):
+    print(i ,items[i])
