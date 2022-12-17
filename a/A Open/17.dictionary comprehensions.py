3e5c3d78a8f36805296d866b79384b1f7d7316c1
May Wu <maywu4@gmail.com>
2022-12-17 13:35:46 -0500

    dictionary comprehensions

--- a/A Open/17.dictionary comprehensions.py
+++ a/A Open/17.dictionary comprehensions.py
@@ -5,6 +5,8 @@ values = [32, "Corina", 1.4]
 #     d[keys[i].title()] = values[i]
 
 #{key: value for-loop}
-d = {keys[i].title(): values[i] for i in range(len(keys))}
+# d = {keys[i].title(): values[i] for i in range(len(keys))}
 
+
+d = {key: value for key, value in zip(keys, values)}
 print(d)
