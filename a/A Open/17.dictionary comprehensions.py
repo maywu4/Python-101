4838e5fc55ec0cbb5bf365442a7bdcc780598c5b
May Wu <maywu4@gmail.com>
2022-12-16 19:15:32 -0500

    dictionary comprehensions

---
+++ a/A Open/17.dictionary comprehensions.py
@@ -0,0 +1,10 @@
+keys = ['age', 'name', 'height']
+values = [32, "Corina", 1.4]
+# d = dict()
+# for i in range(len(keys)):
+#     d[keys[i].title()] = values[i]
+
+#{key: value for-loop}
+d = {keys[i].title(): values[i] for i in range(len(keys))}
+
+print(d)
