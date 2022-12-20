3370c7acffb5f3ebd34765d0aa87077be153d109
May Wu <maywu4@gmail.com>
2022-12-20 11:51:50 -0500

    decorators

--- a/A Open/19.decorators.py
+++ a/A Open/19.decorators.py
@@ -3,5 +3,13 @@ def exponent_function_decorator(exponent_function_to_wrap):
         return base_number ** exponent_function_to_wrap(exponent)
     return wrapper_function
 
+@exponent_function_decorator
 def exponent_function(exponent):
     return int(exponent)
+
+# print(exponent_function)
+print(exponent_function(4, 2.23746))
+# exponent_function = exponent_function_decorator(exponent_function)
+# print(exponent_function)
+# print(exponent_function._closure_[0].cell_contents)
+
