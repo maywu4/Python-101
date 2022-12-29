dac56481297ec1aebafa42ef2c75ac9fd1bc0e5f
May Wu <maywu4@gmail.com>
2022-12-29 18:04:21 -0500

    tuples

---
+++ crash_course/4.Tuples.py
@@ -0,0 +1,19 @@
+dimensions =  (200, 50)
+# print("Original dimensions:")
+for dimension in dimensions:
+    # print(dimension)
+    pass
+
+dimensions = (400, 100)
+# print("\nModified dmensions:")
+for dimension in dimensions:
+    # print(dimension)
+    pass
+
+
+#4-13
+buffet = ('sushi', 'pizza', 'ice-cream', 'bbq', 'hot pot')
+# buffet[2] = 'cake'
+buffet = ('sushi', 'pizza', 'cake', 'bbq', 'salad')
+for food in buffet:
+    print(food)
